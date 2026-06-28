ad5096fe45875860c6c089649085d0747832ff7a
ba138 <basitalyshah51214@gmail.com>
2026-06-28 13:28:43 +0500

    Initial Commit

---
+++ TodoList/TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by Basit Ali on 28/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
